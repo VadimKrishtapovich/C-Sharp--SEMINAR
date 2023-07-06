2fe5bfed83af6d868ccd9ea0056898c8c4dd7260
Vadim <vadim.krishtapovich@mail.ru>
2023-07-06 13:03:51 +0300

    Seminar1 плюс первое домашнее задание

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,4 +1,5 @@
-﻿// int a = 25;
+﻿///* Инструкция для примера.
+// int a = 25;
 // double b = 36.6;
 // string c = "Hello world";
 // bool d = true;
@@ -14,7 +15,7 @@
 
 // Console.WriteLine("Your nubber is " + num);
 
-//Task 1. Напишите программу, которая на вход принимает два числа и проверяет, 
+///* Task 1. Напишите программу, которая на вход принимает два числа и проверяет, 
 // является ли первоечисло квадратом второго.
 
 // Console.Write("Input a first number: ");
@@ -32,35 +33,74 @@
 //     {
 //     Console.Write("No");
 //     }
-////
+// //
 
+///* Task 2 Напишите программу, которая на вход принимает одно число (N), а на выходе 
+// показывает все целые числа в промежутке от -number до number.
 
-// Task 2 Напишите программу, которая на вход принимает 
-// одно число (N), а на выходе показывает все целые числа 
-// в промежутке от -number до number.
-
-
-
-//Task 3. Написать программу которая на вход принимает 
-//два числа и выдаёт большее. (Не до конца). !!!! как-то надо 
+///* Task 3. Написать программу которая на вход принимает два числа и выдаёт большее. 
+// (Не до конца). !!!! как-то надо 
 // вставить макс. Исправить ошибку если второе число больше, 
-// то ставит, что числа ровны.
+// то ставит, что числа ровны. УТОЧНИТЬ КАК ДЕЛЬТЬ ЧТОБЫ МАКСИМАЛЬНОЕ
+// ВВОДИМОЕ ЧИСЛО БЫЛО ТРИ??
+// A = 5;
+// B = 7;
+// max = 7.
 
 
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//         Console.Write("max = " + num1);
+// }
+//     else
+//     {
+//         Console.Write("max = " + num2);
+//     }
 
+// Task 3.1. Написать программу которая на вход принимает два числа и выдаёт большее.
 
-if (num1 > num2)
-{
-        Console.Write("max=" + num1);
-}
-    else
-    {
-        Console.Write("max=" + num2);
-    }
-      
+// A = 2;
+// B = 10;
+// max = 10.
+
+// Console.Write ("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.Write("max = " + num1);
+// }
+// else
+// {
+//     Console.Write("max = " + num2);
+// }
+
+// Task 3.2. Написать задачу которая на вход принимает два числа и выдаёт большее.
+
+// a = -9;
+// b = -3;
+// max = -3.
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.Write("max = " + num1);
+// }
+// else
+// {
+//     Console.Write("max = " + num2);
+// }
+
+///*
