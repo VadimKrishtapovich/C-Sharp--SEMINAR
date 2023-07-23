8e9b588b8849f60da8d72b4de6ec2a1a506be7b6
Vadim <vadim.krishtapovich@mail.ru>
2023-07-23 19:28:11 +0300

    одномерные массивы

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,4 +1,6 @@
-﻿void ShowNumber(int n)
+﻿/* примеры для понимания.
+
+void ShowNumber(int n)
 {
     n += 5;
     Console.WriteLine(n);
@@ -8,3 +10,34 @@ int a = 10;
 
 ShowNumber(a);
 Console.WriteLine(a);
+*/
+
+/* Один и тотже массив с разными именами.
+
+void ShowFirstElement(int [] array)
+{
+    array[0] += 5;
+    Console.WriteLine(array[0]);
+}
+
+int[] myArray = {10, 3, 8};
+
+ShowFirstElement(myArray);
+Console.WriteLine(myArray[0]);
+*/
+
+// Создаём скопированный массив и записываем туда myArray
+
+void ShowFirstElement(int [] array)
+{
+    array[0] += 5;
+    Console.WriteLine(array[0]);
+}
+
+int[] myArray = {10, 3, 8};
+int[] copiedArray = myArray;
+
+ShowFirstElement(copiedArray);
+
+Console.WriteLine(myArray[0]);
+
