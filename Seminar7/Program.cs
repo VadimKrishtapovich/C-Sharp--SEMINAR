4adae019288d8165283a70402cf9456105162dda
Vadim <vadim.krishtapovich@mail.ru>
2023-07-26 17:22:51 +0300

    Двумерные массивы

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -134,5 +134,59 @@ Changer2dArray(myArray);
 Show2dArray(myArray);
 */
 
-// Task 4. Задайте двумерный массив. Найдите сумму элементов, находящихся на 
+/* Task 4. Задайте двумерный массив. Найдите сумму элементов, находящихся на 
 // главной диагонали (с индексами (0,0); (1;1) и т.д. (Аналитический метод).
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for(int i=0; i<rows; i++)
+    
+        for(int j=0; j<columns;j++)
+        
+            array[i,j] = new Random().Next(minValue, maxValue+1);
+        
+    return array;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j]+ " ");
+
+        Console.WriteLine();
+    }
+}
+
+int CountSum(int[,] array)
+{
+    int result = 0;
+    for(int i = 0, j = 0; i<array.GetLength(0) && j<array.GetLength(1); i++ , j++)
+    result = result + array[i,j];
+    return result;
+}
+
+Console.Write("input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a min possible value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a max possible value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int [,] myArray = CreateRandom2dArray(rows, columns, minValue, maxValue);
+
+Show2dArray(myArray);
+
+int result = CountSum(myArray);
+Console.WriteLine(result);
+
+// Пояснение к Task 4...... 
+// Главная диагональ в двумерном массиве (или матрице) - это линия, 
+// которая проходит от верхнего левого угла до нижнего правого угла 
+// и содержит элементы с одинаковыми индексами по строкам и столбцам.
+
+*/
