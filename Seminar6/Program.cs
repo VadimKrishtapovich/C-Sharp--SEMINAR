2b5d201b2b13fd6201bdc1bfecda2212063ada3f
Vadim <vadim.krishtapovich@mail.ru>
2023-07-27 17:21:26 +0300

    одномерные массивы часть 2

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -316,3 +316,82 @@ PrintArray(array);
 Console.Write($" Количество чисел боьше 0: {CountPositiveNumbers(array)}: ");
 
 */
+
+/* Task 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int Coefficient = 0;
+const int Constant = 1;
+const int X_coord = 0;
+const int Y_coord = 1;
+const int Line1 = 1;
+const int Line2 = 2;
+
+double[] lineData1 = InputLineData(Line1);
+double[] lineData2 = InputLineData(Line2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Точка пересечений уравнений y = {lineData1[Coefficient]} * x {lineData1[Constant]} && y = {lineData2[Coefficient]} * x {lineData2[Constant]}");
+    Console.WriteLine($"имеет координаты ({coord[X_coord]}, {coord[Y_coord]})");
+}
+
+// Ввод числа
+
+double Prompt(string message)
+{
+    System.Console.Write(message); // вывести сообщение
+    string value = Console.ReadLine(); // Считывает с консоли строку.
+    int result = Convert.ToInt32(value); //Преобразует строку в целое число
+
+    return result;
+}
+
+// Ввод данных по прямой
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[Coefficient] = Prompt($"Введите коэфициент для {numberOfLine} прямой: ");
+    lineData[Constant] = Prompt($"Введите константу для {numberOfLine} прямой: ");
+
+    return lineData;
+}
+
+// Поиск координат
+
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X_coord] = (lineData1[Constant] - lineData2[Constant])
+    / (lineData2[Coefficient] - lineData1[Coefficient]);
+    coord[Y_coord] = lineData1[Constant] * coord[X_coord]
+    + lineData1[Constant];
+
+    return coord;
+
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if (lineData1[Coefficient] == lineData2[Coefficient])
+    {
+        if (lineData1[Constant] == lineData2[Constant]);
+        {
+            Console.WriteLine("Прямые совпадают");
+
+        }
+        return false;
+    }
+
+    else
+        {
+            Console.WriteLine("Прямые праллельны");
+            return false;
+        }
+        return true;
+}
+
+*/
+
